9572e84a65099957985c55b38e148a6b76a5b368
Jefferson Daniel Castro Aguilar <jedcastroag@unal.edu.co>
2019-04-15 18:13:23 -0500

    Picas y Fijas

---
+++ PicasYFilas/picasFijas.java
@@ -0,0 +1,36 @@
+import java.util.Queue;
+
+/**
+ * picasFijas
+ */
+public class picasFijas {
+
+    int a[] = { 0, 1, 2, 3 };
+    boolean first = false;
+    boolean fijos[] = { false, false, false, false };
+    int ultima_fijas = -0;
+    Queue<int[]> cola = new Queue<>();
+
+    int[] compute(int p, int f) {
+
+        if (f > ultima_fijas) {
+
+        }
+        if (p < 0) {
+            cola.add(a);
+            return a;
+        } else {
+            if (f == 0) {
+                for (int i = 0; i < a.length; i++)
+                    a[i]++;
+                return a;
+            }
+            if (f == 1 && !first) {
+                for (int i = 0; i < a.length; i++)
+                    a[i]++;
+                first = true;
+                return a;
+            }
+        }
+    }
+}
